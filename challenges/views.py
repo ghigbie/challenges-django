1ada2bbc52a1f7c9bc63c9f939f1c824ef422b44
ghigbie <gehigbie@gmail.com>
2021-05-14 09:17:49 -0400

    Add and edit template

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.http.response import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
@@ -5,28 +6,22 @@ from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "celebrate christmas",
-    "feb": "eat goose",
+    "february": "eat goose",
     'march': "hunt the deer",
     "april": "clean the house",
     "june": "clean the house",
-    "july": "master Django!"
+    "july": "master Django!",
+    "august": "stay cold",
+    "september": "stay dry",
+    "october": "eat pumpkin",
+    "november": "avoid birds",
+    "december": "kick a fat man"
 }
 
 def index(request):
-    list_items = ""
-    months = list(monthly_challenges.keys())
-
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month_challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = f"""
-            <ul>
-                {list_items}
-            </ul>
-        """
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": list(monthly_challenges.keys())
+    })
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
