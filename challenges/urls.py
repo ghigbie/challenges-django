3b68e64d12994671c8e87f0e76343cd36e7b9800
ghigbie <gehigbie@gmail.com>
2021-05-14 21:42:22 -0400

    Add more templates

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path("", index),
+    path("", index, name="index"),
+    path("about", about, name="about"),
     path('<int:month>', monthly_challenge_by_number),
     path('<str:month>', monthy_challenge, name="month_challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -3,24 +3,16 @@ from django.http.response import HttpResponse, HttpResponseNotFound, HttpRespons
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.template.loader import render_to_string
-
-monthly_challenges = {
-    "january": "celebrate christmas",
-    "february": "eat goose",
-    'march': "hunt the deer",
-    "april": "clean the house",
-    "june": "clean the house",
-    "july": "master Django!",
-    "august": "stay cold",
-    "september": "stay dry",
-    "october": "eat pumpkin",
-    "november": "avoid birds",
-    "december": "kick a fat man"
-}
+from .data_set import *
 
 def index(request):
     return render(request, "challenges/index.html", {
         "months": list(monthly_challenges.keys())
+    })
+
+def about(request):
+    return render(request, "challenges/about.html", {
+        "data": "this is an about page"
     })
 
 def monthly_challenge_by_number(request, month):
